473303e4c0243386bb97ea51af335483fe08ba1c
Dylan C <hchen004@citymail.cuny.edu>
2017-07-29 17:07:31 -0400

    Initial commit

---
+++ DataModel.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GlassdoorAPI
+{
+    public class FeaturedReview
+    {
+        public string attributionURL { get; set; }
+        public int id { get; set; }
+        public bool currentJob { get; set; }
+        public string reviewDateTime { get; set; }
+        public string jobTitle { get; set; }
+        public string location { get; set; }
+        public string jobTitleFromDb { get; set; }
+        public string headline { get; set; }
+        public string pros { get; set; }
+        public string cons { get; set; }
+        public int overall { get; set; }
+        public int overallNumeric { get; set; }
+    }
+
+    public class Image
+    {
+        public string src { get; set; }
+        public int height { get; set; }
+        public int width { get; set; }
+    }
+
+    public class Ceo
+    {
+        public string name { get; set; }
+        public string title { get; set; }
+        public int numberOfRatings { get; set; }
+        public int pctApprove { get; set; }
+        public int pctDisapprove { get; set; }
+        public Image image { get; set; }
+    }
+
+    public class Logo
+    {
+        public string normalUrl { get; set; }
+        public string mediumUrl { get; set; }
+        public string smallUrl { get; set; }
+    }
+
+    public class ParentEmployer
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string relationshipDate { get; set; }
+        public string relationship { get; set; }
+        public bool isSunset { get; set; }
+        public string sunsetMessage { get; set; }
+        public Logo logo { get; set; }
+    }
+
+    public class Employer
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string website { get; set; }
+        public bool isEEP { get; set; }
+        public bool exactMatch { get; set; }
+        public string industry { get; set; }
+        public int numberOfRatings { get; set; }
+        public string squareLogo { get; set; }
+        public string overallRating { get; set; }
+        public string ratingDescription { get; set; }
+        public string cultureAndValuesRating { get; set; }
+        public string seniorLeadershipRating { get; set; }
+        public string compensationAndBenefitsRating { get; set; }
+        public string careerOpportunitiesRating { get; set; }
+        public string workLifeBalanceRating { get; set; }
+        public int recommendToFriendRating { get; set; }
+        public int sectorId { get; set; }
+        public string sectorName { get; set; }
+        public int industryId { get; set; }
+        public string industryName { get; set; }
+        public FeaturedReview featuredReview { get; set; }
+        public Ceo ceo { get; set; }
+        public ParentEmployer parentEmployer { get; set; }
+    }
+
+    public class Response
+    {
+        public string attributionURL { get; set; }
+        public int currentPageNumber { get; set; }
+        public int totalNumberOfPages { get; set; }
+        public int totalRecordCount { get; set; }
+        public List<Employer> employers { get; set; }
+    }
+
+    [Serializable]
+    public class DataModel
+    {
+        public bool success { get; set; }
+        public string status { get; set; }
+        public string jsessionid { get; set; }
+        public Response response { get; set; }
+    }
+}
---
+++ Glassdoor.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace GlassdoorAPI
+{
+    public class Glassdoor
+    {
+        HashSet<string> dataFormats = new HashSet<string>() { "json", "xml" };
+
+        /// <summary> default constructor, default format: JSON 
+        /// </summary>
+        public Glassdoor() {
+            this.DataFormat = "json";
+        }
+
+        /// <summary> constructor with 1 parameter
+        /// <param name="JobField">search query</param>
+        /// </summery>
+        public Glassdoor(string JobField) {
+            this.DataFormat = "json";
+            this.JobField = JobField;            
+        }
+
+        /// <summary>
+        /// Constructor with 2 parameters
+        /// </summary>
+        /// <param name="JobField"> search query </param>
+        /// <param name="DataFormat"> Data format (JSON or XML) </param>
+        public Glassdoor(string JobField, string DataFormat) {
+            this.JobField = JobField;
+            DataFormat = DataFormat.ToLower();
+            if (dataFormats.Contains(DataFormat))
+                this.DataFormat = DataFormat;
+            else throw new ArgumentException("Wrong data format");
+        }
+
+        /// <summary>
+        /// Search query
+        /// </summary>
+        public string JobField { get; set; }
+
+        /// <summary>
+        /// Data format, JSON or XML
+        /// </summary>
+        public string DataFormat { get; set; }
+
+        /// <summary>
+        /// Make request to the Glassdoor API
+        /// </summary>
+        /// <returns> The body of the response in plain String format </returns>
+        public string Search() {
+            var client = new RestClient(
+               baseUrl: string.Format(
+                    "http://api.glassdoor.com/api/api.htm?v=1&format={0}&t.p={1}&t.k={2}&action=employers&q={3}&userip=192.168.43.42&useragent=Mozilla%2F%2F4.0"
+                    , DataFormat,
+                    GlassdoorCred.PARTNER_ID,
+                    GlassdoorCred.KEY, 
+                    JobField
+                    )
+            );
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+            return response.Content;
+        }
+
+        /// <summary>
+        /// Glassdoor API credentials
+        /// </summary>
+        private static class GlassdoorCred
+        {
+            public static readonly string PARTNER_ID = "[REPLACE]";
+            public static readonly string KEY = "[REPLACE]";
+        }
+    }
+
+    
+}
